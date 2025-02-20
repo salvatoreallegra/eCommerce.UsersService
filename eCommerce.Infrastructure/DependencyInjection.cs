c3b33483a08ae11ea5ba7eb88f6cce7478e4a219
Salvatore Allegra <salallegra@gmail.com>
2025-02-20 00:48:02 -0800

    Create extension to add infrastructure services to IOC container.

---
+++ eCommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
