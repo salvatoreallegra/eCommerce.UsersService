f6a6946e7ce8c785e4f29ab89d46746c4b568fc5
Salvatore Allegra <salallegra@gmail.com>
2025-02-20 00:55:27 -0800

    add core extension method for injecting services

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,7 +1,11 @@
+using eCommerce.Infrastructure;
+using eCommerce.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Core
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
