c5885f5e08a079b8232616cbeb290115cd949cfc
Salvatore Allegra <salallegra@gmail.com>
2025-02-20 01:00:29 -0800

    add routing and authentication middleware

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -19,8 +19,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseRouting();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
