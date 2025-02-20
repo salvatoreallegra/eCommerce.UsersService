269a971773f589f18cb0e72280d69c3ff86584b0
Salvatore Allegra <salallegra@gmail.com>
2025-02-20 12:23:40 -0800

    Added user model and request/response DTO's

---
+++ eCommerce.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Core.Entities
+{
+    /// <summary>
+    /// User model that will store user info in database.
+    /// </summary>
+    public class ApplicationUser
+    {
+        public Guid UserId { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string? PersonName { get; set; }
+        public string? Gender { get; set; }
+    }
+}
